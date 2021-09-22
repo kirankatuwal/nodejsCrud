edec13c7dd6c0ffc1832e04670929e3b9155b489
kirankatuwal <kirankatuwal1@gmail.com>
2021-09-22 18:09:20 +0545

    Checked

---
+++ server.js
@@ -0,0 +1,42 @@
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+// dotenv file for securing credential
+dotenv.config({ path: 'config.env' });
+const PORT = process.env.PORT || 8080;
+
+// log request
+app.use(morgan('tiny'));
+
+// parse request to body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+// app.set("views",path.resolve(__dirname,"views/ejs"));
+
+//load assets
+
+// app.use('/css'.express.static(path.join(__dirname, "assets/css")));
+// app.use('/img'.express.static(path.join(__dirname, "assets/img")));
+// app.use('/js'.express.static(path.join(__dirname, "assets/js")));
+
+app.use('/img', express.static(path.join(__dirname, 'assets/img')));
+app.use('/js', express.static(path.join(__dirname, 'assets/js')));
+app.use('/css', express.static(path.join(__dirname, 'assets/css')));
+
+// set view engine
+app.set('view engine', "ejs")
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server started on http://localhost:${PORT} port`);
+})
+
+
